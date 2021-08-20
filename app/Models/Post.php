9726e3d3dd4aeea38b41134293eb6f1614248834
Salim66 <salimhasanriad8@gmail.com>
2021-08-20 09:21:40 +0600

    Relationship and mass assignment

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // mass assignment
+    protected $guarded = [];
+
+    public function user(){
+        return $this->belongsTo('user_id');
+    }
 }
